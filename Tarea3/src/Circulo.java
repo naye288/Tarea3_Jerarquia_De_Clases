e46d2371b66173960a3d52bfb66cb8ad178a3446
naye288 <79212723+naye288@users.noreply.github.com>
2023-03-16 14:43:10 -0600

    termine las clases de circulo y triangulo
    falta modificar el compareTo

--- Tarea3/src/Circulo.java
+++ Tarea3/src/Circulo.java
@@ -1,3 +1,30 @@
-public class Circulo {
+public class Circulo extends Figura implements Comparable<Circulo>{
+    private double radio;
     
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+    
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+    
+    @Override
+    public int CompareTo(Circulo cirPorComparar) {
+        double areaPorComparar = cirPorComparar.calcularArea();
+        double miArea = this.calcularArea();
+        if(miArea==areaPorComparar){
+            return 0;
+        }else if (miArea > areaPorComparar){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+
+    @Override
+    public int lados() {
+        return 0;
+    }
 }
--- Tarea3/src/Triangulo.java
+++ Tarea3/src/Triangulo.java
@@ -1,3 +1,32 @@
-public class Triangulo {
+public class Triangulo extends Figura implements Comparable<Triangulo>{
+    private double base;
+    private double altura;
+
     
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    @Override
+    public int CompareTo(Triangulo triPorComparar) {
+        double areaPorComparar = triPorComparar.calcularArea();
+        double miArea = this.calcularArea();
+        if(miArea==areaPorComparar){
+            return 0;
+        }else if (miArea > areaPorComparar){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+    @Override
+    public double calcularArea() {
+        return (base * altura) / 2;
+    }
+    @Override
+    public int lados() {
+        return 3;
+    }
+
+
 }
